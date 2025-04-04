9aba472b37be8f10e65b34ffc2d9fd483ce1b2d4
Giovana <giovana.rafaela7@hotmail.com>
2025-04-04 15:50:40 -0300

    :lipstick: feat: Adicionando responsividade e logo

--- Cadastro_Login/cadastro.js
+++ Cadastro_Login/cadastro.js
@@ -1,24 +1,15 @@
-// Seleciona o elemento com a classe 'card'
 let card = document.querySelector('.card');
 
-// Seleciona o botão de login
 let loginButton = document.querySelector('.btnFacaLogin');
 
-// Seleciona o botão de cadastro
 let cadastroButton = document.querySelector('.cadastreSe');
 
-// Adiciona um evento de clique ao botão de login
 loginButton.onclick = () => {
-    // Remove a classe 'cadastroActive' do card
     card.classList.remove('cadastroActive');
-    // Adiciona a classe 'loginActive' ao card
     card.classList.add('loginActive');
 }
 
-// Adiciona um evento de clique ao botão de cadastro
 cadastroButton.onclick = () => {
-    // Remove a classe 'loginActive' do card
     card.classList.remove('loginActive');
-    // Adiciona a classe 'cadastroActive' ao card
     card.classList.add('cadastroActive');
 }
