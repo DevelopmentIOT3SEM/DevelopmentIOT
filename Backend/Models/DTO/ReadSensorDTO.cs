b1b0ee8e554b0bb997209078d6284cb4c1529ac9
b4rbosa777 <matheushb756@gmail.com>
2025-04-29 15:19:57 -0300

    feat: adding sensor

---
+++ Backend/Models/DTO/ReadSensorDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PecaMonitoramentoAPI.DTOs
+{
+    public class ReadSensorDTO
+    {
+        public int IdSensor { get; set; }
+        public string Nome { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/Repositories/SensorRepository.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using PecaMonitoramentoAPI.Data;
+using PecaMonitoramentoAPI.DTOs;
+using PecaMonitoramentoAPI.Repositories.Interfaces;
+
+namespace PecaMonitoramentoAPI.Repositories
+{
+    public class SensorRepository : ISensorRepository
+    {
+        private readonly DapperContext _context;
+
+        public SensorRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<ReadSensorDTO>> GetAllAsync()
+        {
+            var query = @"SELECT 
+                            id_sensor AS IdSensor,
+                            nome AS Nome
+                        FROM sensor";
+
+            using var connection = _context.CreateConnection();
+            return await connection.QueryAsync<ReadSensorDTO>(query);
+        }
+
+        public async Task<ReadSensorDTO?> GetByIdAsync(int id)
+        {
+            var query = @"SELECT 
+                            id_sensor AS IdSensor,
+                            nome AS Nome
+                        FROM sensor
+                        WHERE id_sensor = @id";
+
+            using var connection = _context.CreateConnection();
+            return await connection.QueryFirstOrDefaultAsync<ReadSensorDTO>(query, new { id });
+        }
+    }
+}
