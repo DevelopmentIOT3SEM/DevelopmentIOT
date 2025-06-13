cf46b289ea350d24d6d073b331f95992845808d4
Giovana <giovana.rafaela7@hotmail.com>
2025-06-13 10:58:54 -0300

    :feat: fixing chatbot html and css

--- Frontend/Home/chatbot.js
+++ Frontend/Home/chatbot.js
@@ -44,7 +44,7 @@ function enviarMensagem() {
 
     mostrarLoading();
 
-    fetch("http://localhost:5000/chat", {
+    fetch("http://127.0.0.1:5000/chat", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
