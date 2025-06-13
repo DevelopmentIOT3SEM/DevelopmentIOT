0fd6208a46ea8f092fc306303328724bd82cb650
Giovana <giovana.rafaela7@hotmail.com>
2025-06-13 14:24:59 -0300

    :der and queries:

--- Codigo_SQL/tabelas.sql
+++ Codigo_SQL/tabelas.sql
@@ -1,22 +1,21 @@
 CREATE TABLE Usuarios (
-    Id SERIAL PRIMARY KEY,
-    Nome VARCHAR(100) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    SenhaHash VARCHAR(255) NOT NULL
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL
 );
--- Tabela Peça (DML - Fixa)
+
 CREATE TABLE Peca (
     id_peca SERIAL PRIMARY KEY,
     tipo_material VARCHAR(50) NOT NULL
 );
 
--- Tabela Sensor (DML - Fixa)
+
 CREATE TABLE Sensor (
     id_sensor SERIAL PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
--- Tabela Monitoramento (CR - Dinâmica)
 CREATE TABLE Monitoramento (
     id_monitoramento SERIAL PRIMARY KEY,
     id_sensor INT REFERENCES Sensor(id_sensor),
@@ -24,22 +23,85 @@ CREATE TABLE Monitoramento (
     timestamp_monitoramento TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- Tabela Produção (CR - Dinâmica)
 CREATE TABLE Producao (
     id_producao SERIAL PRIMARY KEY,
-    id_peca INT REFERENCES Peca(id_peca) NOT NULL, -- Agora obrigatório
+    id_peca INT REFERENCES Peca(id_peca) NOT NULL, 
     rampa INT NOT NULL,
     timestamp_producao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- Inserindo em Peça (DML)
 INSERT INTO Peca (id_peca, tipo_material) VALUES 
 (1, 'Metálica'),
 (2, 'Plástica'),
-(3, 'Refugo'); -- Adicionando o tipo Refugo
+(3, 'Refugo'); 
 
--- Inserindo em Sensor (DML)
 INSERT INTO Sensor (id_sensor, nome) VALUES 
 (1, 'Atuador 1'),
 (2, 'Atuador 2'),
 (3, 'Esteira');
+
+-- Queries
+SELECT 
+    p.id_producao,
+    pc.tipo_material,
+    p.rampa,
+    p.timestamp_producao
+FROM 
+    Producao p
+JOIN 
+    Peca pc ON p.id_peca = pc.id_peca;
+
+SELECT 
+    s.nome,
+    COUNT(m.id_monitoramento) AS total_monitoramentos
+FROM 
+    Sensor s
+LEFT JOIN 
+    Monitoramento m ON s.id_sensor = m.id_sensor
+GROUP BY 
+    s.nome;
+
+
+SELECT 
+    s.nome,
+    m.estado,
+    m.timestamp_monitoramento
+FROM 
+    Sensor s
+JOIN 
+    Monitoramento m ON s.id_sensor = m.id_sensor
+WHERE 
+    m.timestamp_monitoramento = (
+        SELECT MAX(m2.timestamp_monitoramento)
+        FROM Monitoramento m2
+        WHERE m2.id_sensor = s.id_sensor
+    );
+
+SELECT 
+    pr.id_producao,
+    pc.tipo_material,
+    pr.timestamp_producao,
+    s.nome AS sensor,
+    m.estado,
+    m.timestamp_monitoramento
+FROM 
+    Producao pr
+JOIN 
+    Peca pc ON pr.id_peca = pc.id_peca
+JOIN 
+    Monitoramento m ON DATE(pr.timestamp_producao) = DATE(m.timestamp_monitoramento)
+JOIN 
+    Sensor s ON m.id_sensor = s.id_sensor
+ORDER BY 
+    pr.timestamp_producao;
+
+SELECT 
+    pc.tipo_material,
+    COUNT(p.id_producao) AS total_produzido
+FROM 
+    Producao p
+JOIN 
+    Peca pc ON p.id_peca = pc.id_peca
+GROUP BY 
+    pc.tipo_material;
+
